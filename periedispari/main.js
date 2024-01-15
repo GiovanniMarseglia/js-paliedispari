905510906747ed97a2aedbec2f926af63b7f6a5f
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-01-15 15:38:25 +0100

    Completed

---
+++ periedispari/main.js
@@ -0,0 +1,44 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+let numero=0
+
+for(let i=0;numero == 0 || numero > 5;i++){
+ numero = +prompt("inserisci un numero da 1 a 5")
+}
+
+
+function randomnum(){
+    let random = Math.floor(Math.random()*(6-1)+1)
+    return random
+}
+
+
+let computer = randomnum()
+console.log(computer)
+
+
+let somma = numero + computer
+console.log(somma)
+
+
+
+let risultato = pariodispari(somma)
+console.log(risultato)
+
+
+function pariodispari(numero){
+
+    if((numero % 2) == 0){
+        return "pari"
+    }else{
+        return "dispari"
+    }
+}
+
+
+
