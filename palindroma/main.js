c53eb8272843c39d5b945c41c049c61705696962
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-01-15 15:17:12 +0100

    Completed

---
+++ palindroma/main.js
@@ -0,0 +1,35 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+let check = Array.from(prompt("inserisci un nome e ti dirò se è palindroma o meno"))
+
+let nome2 = palindroma(check)
+
+console.log(nome2)
+
+
+function palindroma(nome){
+
+    let b = nome.length-1;
+
+    for(let i=0;i < nome.length;i++){
+        if(nome[i]==nome[b]){
+            if((nome[i]==nome[b]) && ((i==nome.length-1) && (b==0))){
+                return "la parola è palindorma"
+            }
+            b--
+        }else{
+            i=nome.length
+            return "la parola non è palindroma"
+        }
+
+        
+    }
+}
+    
+
+
+
